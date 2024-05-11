c529b7f454e7625b200497337458af3790c2093b
Senyafffka <102877546+Senyafffka@users.noreply.github.com>
2024-05-11 12:34:59 +0700

    Add files via upload

---
+++ ИМ_Лаб10/ИМ_Лаб10/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ИМ_Лаб10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(760, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать матч";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(222, 46);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(445, 46);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(217, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(348, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Интенсивность забиваний";
+            // 
+            // Grid
+            // 
+            this.Grid.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.ColumnHeadersVisible = false;
+            this.Grid.Location = new System.Drawing.Point(12, 132);
+            this.Grid.Name = "Grid";
+            this.Grid.RowHeadersVisible = false;
+            this.Grid.RowHeadersWidth = 62;
+            this.Grid.RowTemplate.Height = 28;
+            this.Grid.Size = new System.Drawing.Size(760, 234);
+            this.Grid.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 382);
+            this.Controls.Add(this.Grid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Баскетбол";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView Grid;
+    }
+}
+
---
+++ ИМ_Лаб10/ИМ_Лаб10/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ИМ_Лаб10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] name = new string[]
+            {
+                "1 Боевые кабаны",
+                "2 Рыжие псы",
+                "3 Птицы гордые",
+                "4 Звери дикие"
+            };
+            Grid.RowCount++;
+            for (int i = 0; i < 4; i++)
+            {
+                Grid.RowCount++;
+                Grid.ColumnCount++;
+                Grid.Columns[i].Width = 100;
+            }
+            Grid.Columns[4].Width = 100;
+            for (int i = 1; i < 5; i++) {
+                Grid.Rows[i].Cells[0].Value = name[i - 1];
+                Grid.Rows[0].Cells[i].Value = i;
+                Grid.Rows[i].Cells[i].Style.BackColor = Color.DarkSalmon;
+            }
+        }
+
+        int i = 1, j = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double y1, y2;
+            Random rand = new Random();
+
+            y1 = (double)numericUpDown1.Value;
+            y2 = (double)numericUpDown2.Value;
+
+            double variable1, avariable2;
+            double S1 = 0, S2 = 0;
+            int point1 = 0, point2 = 0;
+
+            while (S1 >= -y1 || S2 >= -y2)
+            {
+                variable1 = rand.NextDouble();
+                avariable2 = rand.NextDouble();
+
+                if (S1 >= -y1)
+                {
+                    S1 += Math.Log(variable1);
+                    point1++;
+                }
+
+                if (S2 >= -y2)
+                {
+                    S2 += Math.Log(avariable2);
+                    point2++;
+                }
+            }
+
+            if (point1 > point2)
+            {
+                i++;
+            }
+            if (point1 < point2)
+            {
+                j++;
+            }
+            if (i >= 5 || j >= 5) { return ; }
+            Grid.Rows[i].Cells[j].Value = point2 + ":" + point1;
+            Grid.Rows[j].Cells[i].Value = point1 + ":" + point2;
+        }
+    }
+}
